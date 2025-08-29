bb065c15b23be051dd60e4b70fd4b8f8e49a9ef9
Juan <tu.email@example.com>
2025-08-29 11:06:13 +0200

    ajuste del boton hamburguesa en pantallas moviles

--- assets/main.js
+++ assets/main.js
@@ -104,3 +104,21 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+// Función de inicialización de Google Translate
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        {
+            pageLanguage: 'es',                  
+            includedLanguages: 'en,fr,de,it',   
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+        },
+        'google_translate_element' 
+    );
+}
+
+(function () {
+    var gtScript = document.createElement('script');
+    gtScript.type = 'text/javascript';
+    gtScript.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    document.body.appendChild(gtScript);
+})();
