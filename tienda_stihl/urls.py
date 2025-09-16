4aad1cb8ae125b2fce2abe31f7dff865cf429583
Juan <tu.email@example.com>
2025-09-16 12:40:53 +0200

    añadir robots.txt

--- tienda_stihl/urls.py
+++ tienda_stihl/urls.py
@@ -18,6 +18,15 @@ urlpatterns = [
         ),
         name="sitemap"
     ),
+    path(
+    "robots.txt",
+    TemplateView.as_view(
+        template_name="robots.txt",
+        content_type="text/plain"
+    ),
+    name="robots"
+),
+
 ]
 
 # Para servir archivos estáticos desde la carpeta assets/ en modo desarrollo
